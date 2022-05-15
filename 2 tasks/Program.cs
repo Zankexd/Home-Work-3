3fd10a7934733c7ece65b6ec665680d553caa21f
Oleksii <zankexd@icloud.com>
2022-05-15 21:42:55 +0200

    lesson

---
+++ 2 tasks/Program.cs
@@ -0,0 +1,8 @@
+﻿void Kub(double x , double y, double z ,double x1 ,double y1 ,double z1)
+{
+   
+    double B = Math.Sqrt(Math.Pow(x1-x,2)+Math.Pow(y1-y,2)+Math.Pow(z1-z,2));
+    Console.WriteLine(B);
+}
+
+Kub(3,1,3,2,6,8);
