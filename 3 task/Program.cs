e6c314c6f8f17c9cc9f602dbe721e44ef011205d
Oleksii <zankexd@icloud.com>
2022-05-15 21:56:50 +0200

    lesson

---
+++ 3 task/Program.cs
@@ -0,0 +1,12 @@
+﻿void kubik (int a)
+{
+    int index= 0;
+while(index<=a)
+{
+    Console.Write(","+Math.Pow(index,3));
+    index++;
+}
+}
+string a =Console.ReadLine();
+int a1 =int.Parse(a);
+kubik (a1);
